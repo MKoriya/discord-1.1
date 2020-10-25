187076c47c8a7a758d623a102e34e946495de67a
MKoriya <meetkoriya13@gmail.com>
2020-10-25 12:23:24 +0530

    bot

--- cogs/serverManage.py
+++ cogs/serverManage.py
@@ -19,53 +19,26 @@ class ServerManage(commands.Cog):
     @commands.command()
     @commands.has_permissions(kick_members=True)
     async def kick(self, ctx, member:discord.Member, *, reason=None):
-        try: 
-            await member.kick(reason=reason)
-        except(commands.MemberNotFound):
-            print('Member Not Found')
-            await ctx.send('Requested Member not Found\nPlease Try Again!!')
-        except(commands.MissingPermissions):
-            await ctx.send('You don\'t have permission to kick members')
-        except:
-            await ctx.send('Something went wrong\nPlease Try Again Later')
-        else:
-            await ctx.send(f'{member.mention} got kicked.')
+        await member.kick(reason=reason)
+        await ctx.send(f'{member.mention} got kicked.')           
 
     @commands.command()
     @commands.has_permissions(ban_members=True)
     async def ban(self, ctx, member:discord.Member, *, reason=None):
-        try:
-            await member.ban(reason=reason)    
-        except(commands.MemberNotFound):
-            print('Member Not Found')
-            await ctx.send('Requested Member not Found\nPlease Try Again!!')
-        except(commands.MissingPermissions):
-            await ctx.send('You don\'t have permission to kick members')
-        except:
-            await ctx.send('Something went wrong\nPlease Try Again Later')
-        else:
-            await ctx.send(f'{member.mention} got Banned.')
+        await member.ban(reason=reason)
+        await ctx.send(f'{member.mention} got Banned.')            
 
     @commands.command()
     @commands.has_permissions(ban_members=True)
     async def unban(self, ctx, *, member):
-        try:
-            banned_users = await ctx.guild.bans()
-            member_name, member_discriminator = member.split('#')   
-            for ban_entry in banned_users:
-                user = ban_entry.user
-                if (user.name, user.discriminator) == (member_name, member_discriminator):
-                    await ctx.guild.unban(user)
-                    await ctx.send(f'Unbanned {user.mention}')
-                    return
-        except(commands.MemberNotFound):
-            print('Member Not Found')
-            await ctx.send('Requested Member not Found\nPlease Try Again!!')
-        except(commands.MissingPermissions):
-            await ctx.send('You don\'t have permission to kick members')
-        except:
-            await ctx.send('Something went wrong\nPlease Try Again Later')
-
+        banned_users = await ctx.guild.bans()
+        member_name, member_discriminator = member.split('#')   
+        for ban_entry in banned_users:
+            user = ban_entry.user
+            if (user.name, user.discriminator) == (member_name, member_discriminator):
+                await ctx.guild.unban(user)
+                await ctx.send(f'Unbanned {user.mention}')
+                return
 
 def setup(client):
     client.add_cog(ServerManage(client))
